57fde3d97ef60221bb003cafcafbbe7e9f54a2a1
Naveen Yadav <nyadav@u.washington.edu>
2016-03-22 15:58:17 -0700

    Added TableViewController, setting up Core Data
    NavigationViewController, DrinkTableViewController, DataStore for
    persistent data

--- barbot/barbot/DrinkTableViewController.swift
+++ barbot/barbot/DrinkTableViewController.swift
@@ -9,6 +9,33 @@
 import Foundation
 import UIKit
 
-class DrinkTableViewController : UITableViewController {
+class DrinkTableViewController : UIViewController, UITableViewDataSource {
     
+    @IBOutlet weak var tableView: UITableView!
+    var drinks = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Drinks"
+        tableView.registerClass(UITableViewCell.self,
+            forCellReuseIdentifier: "Cell")
+    }
+    
+    // MARK: UITableViewDataSource
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return drinks.count
+    }
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath
+        indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell =
+            tableView.dequeueReusableCellWithIdentifier("Cell")
+            
+            cell!.textLabel!.text = drinks[indexPath.row]
+            
+            return cell!
+    }
 }
